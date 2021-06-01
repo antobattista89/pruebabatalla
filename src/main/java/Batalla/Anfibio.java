505113fbce20fb3ac5602b0b93f6540bae5df44a
ANTO <anbattista@alumno.unlam.edu.ar>
2021-06-01 17:38:41 -0300

    primer commit Anto0106

--- src/main/java/Batalla/Anfibio.java
+++ src/main/java/Batalla/Anfibio.java
@@ -14,7 +14,7 @@ import Batalla.enumeradores.TipoDeBatalla;
  * Anfibio deriva de acuatico por que es su pricipal modalidad.
  */
 class Anfibio extends Acuatico {
-    
+    //test anto2
     Double velocidad = 0.0;
 
     public Anfibio(Integer id, String nombre)
